13ff3a74ccd29f978054e29a8acc37b1c686cf2e
Paula Berggren <pbb16a@acu.edu>
2019-11-04 01:14:19 -0600

    this is the mad libs update

--- app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 
@@ -12,11 +11,43 @@ class DisplayMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_message)
 
         // Get the Intent that started this activity and extract the string
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val m_1 = intent.getStringExtra(EXTRA_MES1)
+        val m_2 = intent.getStringExtra(EXTRA_MES2)
+        val m_3 = intent.getStringExtra(EXTRA_MES3)
+        val m_4 = intent.getStringExtra(EXTRA_MES4)
+        val m_5 = intent.getStringExtra(EXTRA_MES5)
+        val m_6 = intent.getStringExtra(EXTRA_MES6)
+        val m_7 = intent.getStringExtra(EXTRA_MES7)
+        val m_8 = intent.getStringExtra(EXTRA_MES8)
+        val m_9 = intent.getStringExtra(EXTRA_MES9)
 
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
+        val textView_m1 = findViewById<TextView>(R.id.textView_m1).apply {
+            text = m_1
+        }
+        val textView_m2 = findViewById<TextView>(R.id.textView_m2).apply {
+            text = m_2
+        }
+        val textView_m3 = findViewById<TextView>(R.id.textView_m3).apply {
+            text = m_3
+        }
+        val textView_m4 = findViewById<TextView>(R.id.textView_m4).apply {
+            text = m_4
+        }
+        val textView_m5 = findViewById<TextView>(R.id.textView_m5).apply {
+            text = m_5
+        }
+        val textView_m6 = findViewById<TextView>(R.id.textView_m6).apply {
+            text = m_6
+        }
+        val textView_m7 = findViewById<TextView>(R.id.textView_m7).apply {
+            text = m_7
+        }
+        val textView_m8 = findViewById<TextView>(R.id.textView_m8).apply {
+            text = m_8
+        }
+        val textView_m9 = findViewById<TextView>(R.id.textView_m9).apply {
+            text = m_9
         }
     }
 }
--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -13,7 +13,16 @@ import android.widget.EditText
 //        setContentView(R.layout.activity_main)
 //    }
 //}
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+//const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+const val EXTRA_MES1 = "com.example.myfirstapp.MES1"
+const val EXTRA_MES2 = "com.example.myfirstapp.MES2"
+const val EXTRA_MES3 = "com.example.myfirstapp.MES3"
+const val EXTRA_MES4 = "com.example.myfirstapp.MES4"
+const val EXTRA_MES5 = "com.example.myfirstapp.MES5"
+const val EXTRA_MES6 = "com.example.myfirstapp.MES6"
+const val EXTRA_MES7 = "com.example.myfirstapp.MES7"
+const val EXTRA_MES8 = "com.example.myfirstapp.MES8"
+const val EXTRA_MES9 = "com.example.myfirstapp.MES9"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +32,36 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
+        val lib_1 = findViewById<EditText>(R.id.lib1)
+        val lib_2 = findViewById<EditText>(R.id.lib2)
+        val lib_3 = findViewById<EditText>(R.id.lib3)
+        val lib_4 = findViewById<EditText>(R.id.lib4)
+        val lib_5 = findViewById<EditText>(R.id.lib5)
+        val lib_6 = findViewById<EditText>(R.id.lib6)
+        val lib_7 = findViewById<EditText>(R.id.lib7)
+        val lib_8 = findViewById<EditText>(R.id.lib8)
+        val lib_9 = findViewById<EditText>(R.id.lib9)
+        val m1 = lib_1.text.toString()
+        val m2 = lib_2.text.toString()
+        val m3 = lib_3.text.toString()
+        val m4 = lib_4.text.toString()
+        val m5 = lib_5.text.toString()
+        val m6 = lib_6.text.toString()
+        val m7 = lib_7.text.toString()
+        val m8 = lib_8.text.toString()
+        val m9 = lib_9.text.toString()
+
+//        val message = "I think I am qualified for this job because I'm " + m1 + ". I got a college education at the "+ m2 + " and I majored in " + m3 + ". I consider myself very " + m4 + " because I'm a " + m5 + ". I have experience using "+m6+ " "+m7+ " and a " + m8 + ". I think these, among many other qualifications, make me right for this " + m9+ "job. Thank you."
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+            putExtra(EXTRA_MES1, m1+".")
+            putExtra(EXTRA_MES2, m2)
+            putExtra(EXTRA_MES3, m3+".")
+            putExtra(EXTRA_MES4, m4)
+            putExtra(EXTRA_MES5, m5+".")
+            putExtra(EXTRA_MES6, m6)
+            putExtra(EXTRA_MES7, m7)
+            putExtra(EXTRA_MES8, m8)
+            putExtra(EXTRA_MES9, m9)
         }
         startActivity(intent)
     }
